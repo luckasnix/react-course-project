02136f745f8826079782180d1f2c78721f6b45a7
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-01-04 23:09:24 -0300

    Página das tarefas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import TodosProvider from './state/todos/Provider'
 import FilterProvider from './state/filter/Provider'
+import TodoApp from './pages/TodoApp/TodoApp'
 
 function App() {
   return (
     <TodosProvider>
       <FilterProvider>
-        <h1>Hello World!</h1>
+        <TodoApp/>
       </FilterProvider>
     </TodosProvider>
   )
---
+++ src/pages/TodoApp/TodoApp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TodoApp() {
+    return (
+        <>
+            <header>TodoCreator</header>
+            <main>TodoList</main>
+            <footer>TodoFilter</footer>
+        </>
+    )
+}
+
+export default TodoApp
