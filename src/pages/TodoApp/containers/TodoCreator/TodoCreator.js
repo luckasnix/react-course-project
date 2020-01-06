2bc9c15992d7d6ae479947ee0b1a8d5ed22827b2
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-01-06 00:03:40 -0300

    Estilização do contêiner de criação das tarefas

--- src/pages/TodoApp/containers/TodoCreator/TodoCreator.js
+++ src/pages/TodoApp/containers/TodoCreator/TodoCreator.js
@@ -1,30 +1,49 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import { useFormik } from 'formik'
 import TodosContext from '../../../../state/todos/Context'
 import * as todosActions from '../../../../state/todos/actions'
+import * as yup from 'yup'
+import styles from './TodoCreator.module.css'
 
 function TodoCreator() {
-    const { todos, dispatchToTodos } = useContext(TodosContext)
-    useEffect(() => {
-        console.log(todos)
-    }, [todos])
-    const formik = useFormik({
+    const { dispatchToTodos } = useContext(TodosContext)
+    const { getFieldProps, touched, errors, isValid, handleSubmit } = useFormik({
         initialValues: {
             title: ''
         },
-        onSubmit: (values) => {
+        validationSchema: yup.object({
+            title: yup.string()
+                .required('Você precisa preencher com uma tarefa.')
+        }),
+        onSubmit: (values, formikBag) => {
             dispatchToTodos(todosActions.addTodo(values.title))
+            formikBag.setFieldValue('title', '', false)
         }
     })
+    const inputTitle = useRef(null)
+    useEffect(() => {
+        inputTitle.current.focus()
+    }, [])
     return (
-        <form onSubmit={formik.handleSubmit}>
+        <form className={styles.container} onSubmit={handleSubmit}>
             <input
+                className={styles.input}
                 type='text'
                 placeholder='Nova tarefa'
                 autoComplete='off'
-                {...formik.getFieldProps('title')}
+                ref={inputTitle}
+                {...getFieldProps('title')}
             />
-            <button type='submit'>Adicionar tarefa</button>
+            {touched.title && errors.title ? (
+                <small className={styles.error}>{errors.title}</small>
+            ) : null}
+            <button
+                className={styles.submit}
+                type='submit'
+                disabled={!isValid}
+            >
+                Adicionar tarefa
+            </button>
         </form>
     )
 }
