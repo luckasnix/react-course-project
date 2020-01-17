340907a15c55f695d2d952282f6fc2d946c78dcd
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-01-17 00:12:34 -0300

    Estilização do componente de seleção da filtragem das tarefas

--- src/pages/TodoApp/containers/TodoFilter/components/TodoSelect/TodoSelect.js
+++ src/pages/TodoApp/containers/TodoFilter/components/TodoSelect/TodoSelect.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import styles from './TodoSelect.module.css'
 
 function TodoSelect({ value, onOptionChange, options }) {
     return (
-        <select value={value} onChange={onOptionChange}>
+        <select
+            className={styles.select}
+            value={value}
+            onChange={onOptionChange}
+        >
             {options.map((option) => {
-                return <option key={option.value} value={option.value}>{option.title}</option>
+                return (
+                    <option
+                        key={option.value}
+                        value={option.value}
+                    >
+                            {option.title}
+                    </option>
+                )
             })}
         </select>
     )
